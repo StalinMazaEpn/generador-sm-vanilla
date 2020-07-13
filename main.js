6a8ae8fa3a631dd5c2ef621e03e496c288e42cfc
StalinMazaEPN <stalinct97@gmail.com>
2020-07-12 21:04:17 -0500

    mejoras en el generador respecto a validaciones

--- main.js
+++ main.js
@@ -6,55 +6,77 @@ function getRandomInt(min, max) {
 } 
 
 //Función para obtener aleatorios de varios numeros
-function generateManyExcludeNumbers(min, max, excludes, numberOfResults = 1){
-	const temp = [];
+function generateManyExcludeNumbers(min, max, excludesArr, numberOfResults = 1){
+    const multiplyFactor = 2;
+    let tempArr = [];
+    let counterExists = 0;
     
     for (let i = 0; i < numberOfResults; i++) {
-    	const numero = getRandomInt(min, max, excludes);
-        const existsOnTemp = temp.some(elem => elem === numero);
-        const existsOnExclude = excludes.some(elem => elem === numero);
+    	const generatedNumber = getRandomInt(min, max, excludesArr);
+        const existsOnTemp = tempArr.some(temp => temp === generatedNumber);
+        const existsOnExclude = excludesArr.some(exclude => exclude === generatedNumber);
 
-  		if (existsOnTemp || existsOnExclude) {
-            i--;
-            console.warn('existsOnTemp')
+  		if (existsOnTemp || existsOnExclude ) {
+            if(counterExists < (numberOfResults *multiplyFactor)){
+                i--;
+            }
+            counterExists++;
             continue;
   		}else{
-        	temp.push(numero);
+        	tempArr.push(generatedNumber);
         }
-        
 	}
-    return temp;
+    return tempArr;
 }
+
+//Prevenir valores negativos
+function preventNegativesValues(event) {
+    const teclaMas = 'NumpadAdd'.toLowerCase();
+    const teclaMenos = 'NumpadSubtract'.toLowerCase();
+    const teclaPor = 'NumpadMultiply'.toLowerCase();
+    const teclaDividir = 'NumpadDivide'.toLowerCase();
+    const teclaSlashs = 'Slash'.toLowerCase();
+    const teclaSeleccionada = event.code.toLowerCase();
+
+    if (teclaSeleccionada == teclaMas || teclaSeleccionada == teclaMenos || teclaSeleccionada == teclaPor || teclaSeleccionada == teclaDividir || teclaSeleccionada == teclaSlashs) {
+        event.preventDefault();
+        return false;
+    }
+    return true;
+}
+
 
 //Evento cuando el contenido cargo
 document.addEventListener('DOMContentLoaded', function(){
     //Evento cuando el formulario es enviado
     document.getElementById('generatorSMForm').addEventListener('submit', (event) =>{
-
-        document.getElementById('generateBtn').disabled = true;
-        document.getElementById('resultado').innerHTML = "";
-
         event.preventDefault();
+        const loadingHtml = '<div class="lds-dual-ring"></div>';
+        const processingTime = 800;
+        document.getElementById('generateBtn').disabled = true;
+        document.getElementById('resultado').innerHTML = loadingHtml;
+        //Obtener los valores
         const minValue = (document.getElementById('minimo').value) ? parseInt(document.getElementById('minimo').value): 1;
         const maxValue = (document.getElementById('maximo').value) ? parseInt(document.getElementById('maximo').value): 100;
         const quantityValue = (document.getElementById('cantidad').value) ? parseInt(document.getElementById('cantidad').value): 1;
         const excludesValue = document.getElementById('exclusion').value;
-
-        const excludesValuesArr = (excludesValue && excludesValue != '') ? excludesValue.split(',')
+        //Filtrar valores no válidos
+        let excludesValuesArr = (excludesValue && excludesValue != '') ? excludesValue.split(',')
         .map(value => parseInt(value))
         .filter(value => {
-            return !isNaN(value) && typeof value === 'number'
-        }) : [];
-
-        let numerosAleatorios = generateManyExcludeNumbers(minValue, maxValue, excludesValuesArr, quantityValue)
-        console.log("Numero Aleatorio", numerosAleatorios)
-
+            return !isNaN(value) && typeof value === 'number' && value >= minValue && value <= maxValue
+        })
+         : [];
+        excludesValuesArr = excludesValuesArr.filter((value,indice) => excludesValuesArr.indexOf(value) === indice)
+        //Llamar a la función para obtener los números aleatorios
+        let randomNumbers = generateManyExcludeNumbers(minValue, maxValue, excludesValuesArr, quantityValue);
         
+        //Mostrar el resultado
         setTimeout(() => {
             document.getElementById('exclusion').value = excludesValuesArr.toString();
-            document.getElementById('resultado').innerHTML = numerosAleatorios.toString();
+            document.getElementById('resultado').innerHTML = '<span>' + randomNumbers.toString() + '</span>';
             document.getElementById('generateBtn').disabled = false;
-        }, 700)
+        }, processingTime)
         
     });
 })
